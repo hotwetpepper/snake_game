bc19d275c76005a49148898066e73130befe6762
Jahor Čerap <cherep.multiman@gmail.com>
2022-02-24 16:14:12 +0300

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,56 @@
+import pygame
+from random import randrange
+
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = (600, 600)
+OBJECT_SIZE = 10
+
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE)
+x = randrange(0, WINDOW_WIDTH, OBJECT_SIZE)
+y = randrange(0, WINDOW_HEIGHT, OBJECT_SIZE)
+body_snake = [(x, y)]
+length_snake = 1
+dx, dy = 0, 0
+fps = 7
+traffic_dict = {"W": (0, -1), "S": (0, 1), "A": (-1, 0), "D": (1, 0)}
+apple = randrange(0, WINDOW_WIDTH, OBJECT_SIZE), randrange(0, WINDOW_HEIGHT, OBJECT_SIZE)
+
+while True:
+    screen.fill(pygame.Color('black'))
+
+    for i, j in body_snake:
+        pygame.draw.rect(screen, pygame.Color('green'), (i, j, OBJECT_SIZE, OBJECT_SIZE))
+
+    pygame.draw.rect(screen, pygame.Color('red'), (*apple, OBJECT_SIZE, OBJECT_SIZE))
+
+    x += dx * OBJECT_SIZE
+    y += dy * OBJECT_SIZE
+    body_snake.append((x, y))
+    body_snake = body_snake[-length_snake:]
+
+    key = pygame.key.get_pressed()
+
+    if key[pygame.K_w] and (dx, dy) != traffic_dict['S']:
+        dx, dy = traffic_dict['W']
+    if key[pygame.K_s] and (dx, dy) != traffic_dict['W']:
+        dx, dy = traffic_dict['S']
+    if key[pygame.K_a] and (dx, dy) != traffic_dict['D']:
+        dx, dy = traffic_dict['A']
+    if key[pygame.K_d] and (dx, dy) != traffic_dict['A']:
+        dx, dy = traffic_dict['D']
+
+    if x < 0 or x > WINDOW_WIDTH or y < 0 or y > WINDOW_HEIGHT:
+        break
+    # Поедание змейкой самой себя
+    if len(body_snake) != len(set(body_snake)):
+        break
+        
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    # Обновление экрана
+    pygame.display.flip()
+    # Управление частотой кадров
+    clock = pygame.time.Clock()
+    # Количество раз выполнения цикла в секунду
+    clock.tick(2*fps)
